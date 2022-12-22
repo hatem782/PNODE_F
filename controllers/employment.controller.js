fe1da463a99e23fa78c450c18e45313f09ecbce9
talel <talelbriki15@gmail.com>
2022-12-22 21:38:46 +0100

    mergin from main

--- controllers/employment.controller.js
+++ controllers/employment.controller.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const SocieteModel = require("../models/Societe.module");
 const studentModule = require("../models/student.module");
 var mongoose = require('mongoose');
+const PositionModel = require("../models/Position.model");
 
 
 const CreateSociete = async (req, res) => {
@@ -44,10 +45,40 @@ const getAllSociete = async (req, res) => {
     // #swagger.description = 'Endpoint return list of entreprises '
 }
 
-const startPositionInSociete = async () => {
+const startPositionInSociete = async (req,res) => {
     // #swagger.tags = ['employement apis']
     // #swagger.description = 'affect an alumini to a new position in an entreprise ( societe) '
+    try {
+        const {
+            designation,
+            societe,
+            alumini,
+            startDate,
+            endDate
+        } = req.body;
+    /*     const exist = await PositionModel.findOne({ title });
+        if (exist)
+            return res.status(409).json({
+                Message: "Societe already exist",
+                Success: false,
+            }); */
+        const newSociete = new PositionModel({
+            designation
+        });
+        const createdSociete = await newSociete.save();
+        return res.status(200).json({
+            Message: "Societe created suucessfully",
+            Success: true,
+            data: createdSociete,
+        });
 
+
+
+    }
+    catch (error) {
+        console.log("##########:", error);
+        res.status(500).send({ Message: "Server Error", Error: error.message });
+    }
 
 }
 
